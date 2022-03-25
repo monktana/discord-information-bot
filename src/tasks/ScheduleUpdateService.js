d5f9a64f8f83edf5ea59f7c33923214cb505c14d
monktana <svengiebelmann@gmail.com>
2022-03-25 16:34:55 +0100

    feat: further seperated parts of update into own classe/objects

--- src/tasks/ScheduleUpdateService.js
+++ src/tasks/ScheduleUpdateService.js
@@ -1,47 +1,44 @@
 import { MessageEmbed } from "discord.js";
+import { WebhookClient } from "discord.js";
 import got from 'got';
 import { load } from 'cheerio';
 import moment from 'moment';
 
-export default class ScheduleUpdateService {
-  NOTIFICATIONS = {
-    name: {
-      priority: 50,
-      compare: (current, cached) => current.name !== cached.name,
-      handle: undefined,
-    },
-    date: {
-      priority: 10,
-      compare: (current, cached) => current.date.isSame(cached.date),
-      handle: this.sendTextNotification,
-    },
-    isLive: {
-      priority: 1,
-      compare: (current, cached) => current.isLive !== cached.isLive,
-      handle: this.sendEmbedNotification,
-    },
-    title: {
-      priority: 40,
-      compare: (current, cached) => current.title !== cached.title,
-      handle: undefined,
-    },
-    link: {
-      priority: 50,
-      compare: (current, cached) => current.link.href !== cached.link.href,
-      handle: undefined,
-    },
-    thumbnail: {
-      priority: 50,
-      compare: (current, cached) => current.thumbnail.href !== cached.thumbnail.href,
-      handle: undefined,
-    },
-    icon: {
-      priority: 50,
-      compare: (current, cached) => current.icon.href !== cached.icon.href,
-      handle: undefined,
-    }
-  }
+const NOTIFICATION_TYPES = Object.freeze({
+  EMBED: 1,
+  TEXT: 2
+})
 
+const NOTIFICATION_PRIORITY = Object.freeze({
+  HIGH: 1,
+  MEDIUM: 50,
+  LOW: 100
+})
+
+const NOTIFICATIONS = {
+  name: {
+    type: NOTIFICATION_TYPES.TEXT,
+    priority: NOTIFICATION_PRIORITY.LOW,
+    compare: (current, cached) => current.name !== cached.name,
+  },
+  date: {
+    type: NOTIFICATION_TYPES.TEXT,
+    priority: NOTIFICATION_PRIORITY.MEDIUM,
+    compare: (current, cached) => !current.date.isSame(cached.date),
+  },
+  isLive: {
+    type: NOTIFICATION_TYPES.EMBED,
+    priority: NOTIFICATION_PRIORITY.HIGH,
+    compare: (current, cached) => current.isLive !== cached.isLive,
+  },
+  title: {
+    type: NOTIFICATION_TYPES.TEXT,
+    priority: NOTIFICATION_PRIORITY.MEDIUM,
+    compare: (current, cached) => current.title !== cached.title,
+  }
+}
+
+export class ScheduleUpdateService {
   constructor(dependencies) {
     this.dependencies = dependencies;
   }
@@ -50,63 +47,76 @@ export default class ScheduleUpdateService {
     const streams = await ScheduleService.get();
     const changedStreams = []
 
-    this.dependencies.client.connect();
+    this.dependencies.redis.connect();
+
     const promises = streams.map(async (stream) => {
-      const cachedStream = await this.dependencies.client.json.get(stream.link, '.');
+      const cachedStream = await this.dependencies.redis.json.get(stream.link, '.');
 
       if (!cachedStream) {
-        this.dependencies.client.json.set(stream.link, '.', stream);
+        this.dependencies.redis.json.set(stream.link, '.', stream);
         return
       }
 
       cachedStream.date = moment(cachedStream.date)
-
       let changes = this.streamHasChanges(cachedStream, stream)
-      if (!changes) {
+      if (changes.length === 0) {
         return
       }
 
-      changes = changes.sort((first, second) => this.#NOTIFICATIONS[first].priority  - this.#NOTIFICATIONS[second].priority)
-      changedStreams.push({stream, changes})
+      changes = changes.sort((first, second) => NOTIFICATIONS[first].priority  - NOTIFICATIONS[second].priority)
+      const type = NOTIFICATIONS[changes[0]].type
 
-      this.dependencies.client.json.set(stream.link, '.', stream);
+      changedStreams.push({stream, changes, type})
+
+      this.dependencies.redis.json.set(stream.link, '.', stream);
     })
 
     await Promise.all(promises);
 
-    this.sendNotifications(changedStreams);
+    return changedStreams;
   }
 
   streamHasChanges(cached, stream) {
-    return Object.keys(this.#NOTIFICATIONS).filter(key => this.#NOTIFICATIONS[key].compare(stream, cached));
+    return Object.keys(NOTIFICATIONS).filter(key => NOTIFICATIONS[key].compare(stream, cached));
   }
+}
 
-  sendNotifications(updates) {
-    updates.forEach(update => {
-      const change = update.changes[0]
-      this.#NOTIFICATIONS[change].handle.call(this, update.stream)
+export class DiscordNotificationService {
+  constructor(options) {
+    this.webhook = new WebhookClient({ id: options.id, token: options.token});
+  }
+  
+  sendNotifications(notifications) {
+    notifications.forEach(notification => {
+      if (notification.type === NOTIFICATION_TYPES.TEXT) {
+        this.sendTextNotification(notification)
+      }
+
+      if (notification.type === NOTIFICATION_TYPES.EMBED) {
+        this.sendEmbedNotification(notification)
+      }
     });
   }
 
-  sendTextNotification(stream) {
-    this.dependencies.webhookClient.send({
-      content: `UPDATE: ${stream.title} (${stream.name}) will go live at ${stream.date.format('DD MM YYYY hh:mm:ss')}`,
+  sendTextNotification(notification) {
+    this.webhook.send({
+      content: `UPDATE: ${notification.stream.title} (${notification.stream.name}) will go live at ${notification.stream.date.format('DD MM YYYY hh:mm:ss')}`,
       username: 'Hololiver',
       avatarURL: 'https://i.imgur.com/AfFp7pu.png'
     });
   }
-
-  sendEmbedNotification(stream) { 
-    const embed = new MessageEmbed()
-                      .setColor(stream.isLive ? '#f00a2c' : '#bab8b9')
-                      .setTitle(stream.title)
-                      .setURL(stream.link)
-                      .setAuthor({ name: vtuber, iconURL: stream.icon})
-                      .addField('Status', stream.isLive ? 'Live' : 'Finished')
-                      .setImage(stream.thumbnail)
-                      .setTimestamp()
   
-    this.dependencies.webhookClient.send({
+  sendEmbedNotification(notification) {
+    const embed = new MessageEmbed()
+        .setColor(notification.stream.isLive ? '#f00a2c' : '#bab8b9')
+        .setTitle(notification.stream.title)
+        .setURL(notification.stream.link)
+        .setAuthor({ name: vtuber, iconURL: notification.stream.icon})
+        .addField('Status', notification.stream.isLive ? 'Live' : 'Finished')
+        .setImage(notification.stream.thumbnail)
+        .setTimestamp()
+
+    this.webhook.send({
       content: 'The status of a stream changed',
       username: 'Hololiver',
       avatarURL: 'https://i.imgur.com/AfFp7pu.png',
@@ -127,6 +137,11 @@ class ScheduleService {
   
     await Promise.all(streamLinks.map(async (element) => {
       const $element = $(element);
+      const $parent = $element.parent();
+
+      if ($parent.attr('class').includes('carousel')) {
+        return
+      }
   
       const startTime = $element.find("div.datetime").text().trim();
       let $currentContainer = $element.closest("div.container");
--- src/tasks/schedule.js
+++ src/tasks/schedule.js
@@ -1,15 +1,12 @@
 import redis from "redis";
-import { WebhookClient } from "discord.js";
-import ScheduleUpdateService from "./ScheduleUpdateService.js";
+import {ScheduleUpdateService, DiscordNotificationService} from "./ScheduleUpdateService.js";
 
-const client = redis.createClient();
-const webhookClient = new WebhookClient({ id: process.env.WEBHOOK_ID, token: process.env.WEBHOOK_TOKEN});
+const client = redis.createClient()
 
-const scheduleService = new ScheduleUpdateService({
-  client,
-  webhookClient
-})
+const scheduleService = new ScheduleUpdateService({redis: client})
+const discordNotificationService = new DiscordNotificationService({ id: process.env.WEBHOOK_ID, token: process.env.WEBHOOK_TOKEN})
 
-await scheduleService.update();
+const updates = await scheduleService.update()
+discordNotificationService.sendNotifications(updates)
 
 client.quit();
