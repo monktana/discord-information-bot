c8abb8b19e4f2bfa5fe160dec8bda790c0538d1b
monktana <svengiebelmann@gmail.com>
2022-04-01 21:44:52 +0200

    test: case to test addition of date

--- src/tasks/__tests__/ScheduleService.js
+++ src/tasks/__tests__/ScheduleService.js
@@ -8,60 +8,34 @@ const __dirname = dirname(__filename);
 const FIXTURE_PATH = `${__dirname}/fixture`;
 
 const scheduleService = new ScheduleService();
-const schedule = JSON.parse(fs.readFileSync(`${FIXTURE_PATH}/expectedSchedule.json`));
-const streamDays = JSON.parse(fs.readFileSync(`${FIXTURE_PATH}/expectedStreamDays.json`));
+const rawstreamsFixture = JSON.parse(fs.readFileSync(`${FIXTURE_PATH}/rawStreams.json`));
+const stremsWithDatesFixture = JSON.parse(fs.readFileSync(`${FIXTURE_PATH}/stremsWithDates.json`));
+const streamDaysFixture = JSON.parse(fs.readFileSync(`${FIXTURE_PATH}/streamDays.json`));
 
-describe("step one", () => {
-  it("generates the expected stream day information", () => {
-    const html = fs.readFileSync(`${FIXTURE_PATH}/withoutCarousel.html`);
-    const days = scheduleService.stepOne(html);
-
-    expect(days).toStrictEqual(streamDays);
-  });
-
-  it("generates the expected stream day information with carousel", () => {
-    const html = fs.readFileSync(`${FIXTURE_PATH}/withCarousel.html`);
-    const days = scheduleService.stepOne(html);
-
-    expect(days).toStrictEqual(streamDays);
-  });
-});
-
-describe("step two", () => {
-  const html = fs.readFileSync(`${FIXTURE_PATH}/withCarousel.html`);
-  const streamDays = scheduleService.stepOne(html);
-
-  it("generates the expected stream information", () => {
-    const html = fs.readFileSync(`${FIXTURE_PATH}/withCarousel.html`);
-    const streams = scheduleService.stepTwo(streamDays);
-
-    expect(streams).toStrictEqual(schedule);
-  });
-});
-
-
-describe("schedule parsing", () => {
+describe("parse schedule HTML to basic JSON", () => {
   it("generates the expected stream information", () => {
     const html = fs.readFileSync(`${FIXTURE_PATH}/withoutCarousel.html`);
-    const streams = scheduleService.parseScheduleToJson(html);
+    const rawStreams = scheduleService.parseHTMLToJson(html);
 
-    expect(streams).toStrictEqual(schedule);
+    expect(rawStreams).toStrictEqual(rawstreamsFixture);
+    expect(scheduleService.processContext.days).toStrictEqual(streamDaysFixture);
   });
 
   it("generates the expected stream information with carousel", () => {
     const html = fs.readFileSync(`${FIXTURE_PATH}/withCarousel.html`);
-    const streams = scheduleService.parseScheduleToJson(html);
+    const rawStreams = scheduleService.parseHTMLToJson(html);
 
-    expect(streams).toStrictEqual(schedule);
+    expect(rawStreams).toStrictEqual(rawstreamsFixture);
+    expect(scheduleService.processContext.days).toStrictEqual(streamDaysFixture);
   });
+});
 
-  it("generates the same stream information with and without carousel", () => {
-    const htmlWithCarousel = fs.readFileSync(`${FIXTURE_PATH}/withCarousel.html`);
-    const htmlWithoutCarousel = fs.readFileSync(`${FIXTURE_PATH}/withoutCarousel.html`);
+describe("add day information to stream(s)", () => {
+  const html = fs.readFileSync(`${FIXTURE_PATH}/withCarousel.html`);
+  const rawStreams = scheduleService.parseHTMLToJson(html);
 
-    const streamsWithCarousel = scheduleService.parseScheduleToJson(htmlWithCarousel);
-    const streamsWithoutCarousel = scheduleService.parseScheduleToJson(htmlWithoutCarousel);
-
-    expect(streamsWithCarousel).toStrictEqual(streamsWithoutCarousel);
+  it("generates the expected stream information", () => {
+    const streamsWithDates = scheduleService.addStreamDate(rawStreams);
+    expect(streamsWithDates).toStrictEqual(stremsWithDatesFixture);
   });
-})
+});
